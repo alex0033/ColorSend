6f4bbd43ec8c13d5bbe7e19b234bdc116974af06
alex0033 <chanchanmen0807@icloud.com>
2020-06-20 08:34:52 +0900

    rails g controller users index and show again

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root 'basic_pages#home'
   devise_for :users, controllers: {
     :omniauth_callbacks => 'users/omniauth_callbacks',
