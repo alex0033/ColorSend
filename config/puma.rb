f7ee97bee8ff4ebd213ca01d18dd573278595bcb
alex0033 <chanchanmen0807@icloud.com>
2020-06-23 13:29:45 +0900

    puma.rb　本番環境のみでSSL

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-# port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
@@ -11,8 +11,8 @@ plugin :tmp_restart
 
 
 # local環境のSSL化
-if Rails.env.development?
-  cert = ENV['CERT_PATH']
-  key  = ENV['KEY_PATH']
-  ssl_bind "0.0.0.0", 3001, cert: cert, key: key
-end
+# if Rails.env.development?
+#   cert = ENV['CERT_PATH']
+#   key  = ENV['KEY_PATH']
+#   ssl_bind "0.0.0.0", 3001, cert: cert, key: key
+# end
